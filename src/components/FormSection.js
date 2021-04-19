d596f18c4ee433dde0efcefe92ffb871bf6f16ff
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 14:14:46 +0000

    FormSection.js: changed

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -87,7 +87,30 @@ export default class FormSection extends React.Component {
                 							))
                 						)}
                 						<div className={classNames('form-submit', {'mt-3': form_is_inline === false, 'mx-auto': form_is_inline === true, 'mr-xs-0': form_is_inline === true, 'ml-xs-1': form_is_inline === true})}>
-                							<button type="submit" className="btn btn--primary">{_.get(section, 'submit_label', null)}</button>
+                							<form name="simpleContactForm" method="POST" id="simple-contact-form" class="contact-form">
+    <p class="form-row">
+        <label id="contact-form-name-label" for="contact-form-name" class="form-label">Name</label>
+        <input type="text" name="name" id="contact-form-name" aria-labelledby="contact-form-name-label" class="form-input" />
+    </p>
+    <p class="form-row">
+        <label id="contact-form-email-label" for="contact-form-email" class="form-label">Email address</label>
+        <input type="email" name="email" id="contact-form-email" aria-labelledby="contact-form-email-label" class="form-input" />
+    </p>
+    <p class="form-row">
+        <label id="contact-form-message-label" for="contact-form-message" class="form-label">Message</label>
+        <textarea
+            name="message"
+            id="contact-form-message"
+            aria-labelledby="contact-form-message-label"
+            class="form-textarea"
+            rows="7"
+        ></textarea>
+    </p>
+    <p class="form-row form-submit">
+        <button type="submit" class="button">Send Message</button>
+    </p>
+</form>
+
                 						</div>
                 					</div>
                 				</form>
