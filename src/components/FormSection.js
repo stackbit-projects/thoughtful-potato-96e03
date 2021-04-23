630a3eda6ab3175325258ca378b2444055a59224
Stackbit Code Editor <projects@stackbit.com>
2021-04-23 17:39:41 +0000

    FormSection.js: changed

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -8,7 +8,7 @@ export default class FormSection extends React.Component {
 
     document.querySelector("form").addEventListener("submit", handleSubmit);
 
-const handleSubmit = (e) => {
+ handleSubmit = (e) => {
   e.preventDefault()
   let myForm = document.getElementById('pizzaOrder');
   let formData = new FormData(myForm)
